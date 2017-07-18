5ef61cd3fd556b60661427ad48f344142738b7a8
cescoferraro <francescoaferraro@gmail.com>
2017-07-18 19:03:42 -0300

    Baby steps

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const org = require('org');
+const loaderUtils = require('loader-utils');
+const cheerio = require('cheerio');
+
+
+module.exports = function OrgLoader(content) {
+  const query = loaderUtils.getOptions(this) || {};
+  const convertOptions = {
+    exportFromLineNumber: query.exportFromLineNumber || true,
+    headerOffset: query.headerOffset || 1,
+    suppressAutoLink: query.suppressAutoLink || false,
+    suppressSubScriptHandling: query.suppressSubScriptHandling || false,
+  };
+  const parserOptions = {
+    toc: query.toc || false,
+    tocHtml: query.tocHtml || false,
+  };
+  const parser = new org.Parser(parserOptions);
+  const orgDocument = parser.parse(content);
+  const orgHTMLDocument = orgDocument.convert(org.ConverterHTML, convertOptions);
+  const $ = cheerio.load(orgHTMLDocument.contentHTML);
+  if (query.removeTitle) {
+    $('body h1').remove();
+  }
+  return `module.exports = ${JSON.stringify($('body').html())}`;
+};
